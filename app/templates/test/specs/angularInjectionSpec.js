af75317085a004fafc362bd2a81d0320dc0feda8
hung-phan <colorvisavn@gmail.com>
2014-02-17 20:53:05 +0700

    Add mixin function and test

--- app/templates/test/specs/angularInjectionSpec.js
+++ app/templates/test/specs/angularInjectionSpec.js
@@ -1,17 +1,23 @@
-define(['angular', 'angular-mocks'], function(angular) {
+define([
+    'loadingSpec'
+], function() {
     describe('angular template testing', function() {
         var element;
         var $scope;
-
-        beforeEach(inject( function($compile, $rootScope) {
-            $scope = $rootScope;
-            element = angular.element("<div>{{2 + 2}}</div>");
-            element = $compile(element)($rootScope);
+        beforeEach(module('webApp'));
+        beforeEach(inject( function($compile, _$rootScope_) {
+            $scope = _$rootScope_;
+            element = angular.element("<div paggie>[[2 + 2]]</div>");
+            element = $compile(element)($scope);
         } ));
 
         it('should do something useful', function() {
             $scope.$digest();
             expect(element.html()).toBe("4");
+        });
+
+        it('should do something more useful', function() {
+            expect(element.hasClass("plain")).toBe(true);
         });
     });
 });
