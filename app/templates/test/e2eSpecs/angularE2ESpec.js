07f61f092d7fa95960a505f210f7d8ca3e322581
hung-phan <colorvisavn@gmail.com>
2014-03-08 10:18:26 +0700

    Add spec for angular testing with karma

--- app/templates/test/e2eSpecs/angularE2ESpec.js
+++
@@ -1,7 +0,0 @@
-describe('frontpage', function() {
-    var ptor = protractor.getInstance();
-    it('should get the title', function() {
-        ptor.get('/#');
-        expect(ptor.getTitle()).toBe('generator-angular-with-require');
-    });
-});
