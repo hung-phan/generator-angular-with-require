a686a23d87e5add7dcb984df03f528d5c6cd7470
hung-phan <colorvisavn@gmail.com>
2014-03-16 11:44:33 +0700

    concat css file after building

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function(grunt) {
             },
             compass: {
                 files: ['<%%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server', 'autoprefixer']
+                tasks: ['compass:server', 'autoprefixer', 'concat']
             },
             //scripts: {
             //files: ['<%%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
