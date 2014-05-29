a4a583624f58033c85ed5a3a39143b392ec1bf08
hung-phan <colorvisavn@gmail.com>
2014-05-30 01:35:40 +0700

    update htmlmin process

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -294,7 +294,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%%= yeoman.dist %>',
-                    src: '{,*/}*.html',
+                    src: '**/*.html',
                     dest: '<%%= yeoman.dist %>'
                 }]
             }
