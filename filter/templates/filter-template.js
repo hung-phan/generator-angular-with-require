0f8b670a21ca48dc47ab25d6ace590f864683763
hung-phan <colorvisavn@gmail.com>
2014-05-30 12:40:06 +0700

    working on filter template

--- filter/templates/filter-template.js
+++ filter/templates/filter-template.js
@@ -1,10 +1,9 @@
 define(['angular'], function(angular) {
     'use strict';
-    /* Filters */
-
-    angular.module('<%= name %>Filters', []).filter('numberFilter', function() {
-        return function(input) {
-            return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        };
-    });
+    angular.module('<%= name %>Filters', [])
+        .filter('numberFilter', function() {
+            return function(input) {
+                return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+            };
+        });
 });
