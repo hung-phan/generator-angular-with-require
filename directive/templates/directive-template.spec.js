6b461f2922d331602de56f8d2cf2f11f7c6ff3dd
hung-phan <colorvisavn@gmail.com>
2014-05-30 12:18:19 +0700

    test directive template

---
+++ directive/templates/directive-template.spec.js
@@ -0,0 +1,19 @@
+define(['angular-mocks', '<%= name %>'], function() {
+    describe('directive with templateUrl', function() {
+        var element;
+        var $scope;
+        beforeEach(module('<%= name %>Module'));
+        beforeEach(module('<%= name %>/<%= name %>.tpl.html'));
+        beforeEach(inject(function($compile, _$rootScope_) {
+            $scope = _$rootScope_;
+            element = angular.element("<<%=name %>></<%= name%>>");
+            element = $compile(element)($scope);
+        }));
+
+        it('template should be loaded', function() {
+            $scope.$digest();
+            expect(element.html()).toBe('This is directive for dummy');
+        });
+    });
+
+});
