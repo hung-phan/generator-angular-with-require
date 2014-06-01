9ffe74903fc11f89658ae66891eaf9708f4b5064
hung-phan <colorvisavn@gmail.com>
2014-06-02 00:26:57 +0700

    fix bug in gruntfile.js template and update filter spec

--- filter/templates/filter-template.spec.js
+++ filter/templates/filter-template.spec.js
@@ -2,8 +2,8 @@ define(['angular-mocks', '<%= name %>'], function() {
     describe('filter 100000', function() {
         var <%= name %>Filter;
         beforeEach(module('<%= name %>Module'));
-        beforeEach(inject(function(_$injector_) {
-            <%= name %>Filter = _$injector_.get('<%= name %>Filter');
+        beforeEach(inject(function(_$filter_) {
+            <%= name %>Filter = _$filter_('<%= name %>Filter');
         }));
 
         it('should be equal 100,000', function() {
