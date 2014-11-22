5c3a19966cfa3f20e8e82cb7bd0257369e456839
hung-phan <colorvisavn@gmail.com>
2014-11-22 14:49:14 +0700

    Update e2e spec config

--- app/templates/e2e.conf.js
+++ app/templates/e2e.conf.js
@@ -2,6 +2,15 @@ exports.config = {
     seleniumPort: null,
     baseUrl: 'http://localhost:9000',
     rootElement: 'html',
+    onPrepare: function() {
+        // implicit and page load timeouts
+        browser.manage().timeouts().pageLoadTimeout(40000);
+        browser.manage().timeouts().implicitlyWait(25000);
+
+        // for non-angular page
+        browser.ignoreSynchronization = true;
+        // sign in before all tests
+    },
     capabilities: {
         'browserName': 'chrome'
     },
